ef9069f661f308083a35e62dfee77c0c75d454e9
dominiksta <dominik.stahmer@posteo.de>
2022-01-23 15:09:32 +0100

    refactor: basic object orientation (much more left to do)

--- index.js
+++ index.js
@@ -1,91 +1,126 @@
-var strokeWidth = 2;
-var bufferSize;
+class SVGCanvas {
 
-var svgElement = document.getElementById("svgElement");
-var rect = svgElement.getBoundingClientRect();
-var path = null;
-var strPath;
-var buffer = []; // Contains the last positions of the mouse cursor
+    /** The Canvas Element that will be drawn on */
+    _svgElement = null;
+    /** The bounding rectangle of `_svgElement` */
+    _rect = null;
 
-svgElement.addEventListener("mousedown", function (e) {
-    bufferSize = document.getElementById("cmbBufferSize").value;
-    path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    path.setAttribute("fill", "none");
-    path.setAttribute("stroke", "#000");
-    path.setAttribute("stroke-width", strokeWidth);
-    buffer = [];
-    var pt = getMousePosition(e);
-    appendToBuffer(pt);
-    strPath = "M" + pt.x + " " + pt.y;
-    path.setAttribute("d", strPath);
-    svgElement.appendChild(path);
-});
+    /** The width of a stroke */
+    _strokeWidth = 2;
+    /** Buffer size for line smoothing */
+    mouseBufferSize = 4;
+    /** Buffer for smoothing. Contains the last positions of the mouse cursor */
+    _mouseBuffer = [];
 
-svgElement.addEventListener("mousemove", function (e) {
-    if (path) {
-        appendToBuffer(getMousePosition(e));
-        updateSvgPath();
+    /** The svg path for the current line */
+    _path = null;
+    /** The stroke path for the current line */
+    _pathStroke = "";
+
+    constructor(svgElement) {
+        this._svgElement = svgElement;
+        this._rect = svgElement.getBoundingClientRect();
+
+        this._svgElement.addEventListener("mousedown", this._onMouseDown.bind(this));
+        this._svgElement.addEventListener("mousemove", this._onMouseMove.bind(this));
+        this._svgElement.addEventListener("mouseup", this._onMouseUp.bind(this));
     }
-});
 
-svgElement.addEventListener("mouseup", function () {
-    if (path) {
-        path = null;
+    _onMouseDown(e) {
+        this._path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        this._path.setAttribute("fill", "none");
+        this._path.setAttribute("stroke", "#000");
+        this._path.setAttribute("stroke-width", this._strokeWidth);
+        this._mouseBuffer = [];
+        var pt = this._getMousePosition(e);
+        this._appendToBuffer(pt);
+        this._pathStroke = "M" + pt.x + " " + pt.y;
+        this._path.setAttribute("d", this._pathStroke);
+        this._svgElement.appendChild(this._path);
     }
-});
 
-var getMousePosition = function (e) {
-    return {
-        x: e.pageX - rect.left,
-        y: e.pageY - rect.top
-    };
-};
-
-var appendToBuffer = function (pt) {
-    buffer.push(pt);
-    while (buffer.length > bufferSize) {
-        buffer.shift();
-    }
-};
-
-// Calculate the average point, starting at offset in the buffer
-var getAveragePoint = function (offset) {
-    var len = buffer.length;
-    if (len % 2 === 1 || len >= bufferSize) {
-        var totalX = 0;
-        var totalY = 0;
-        var pt, i;
-        var count = 0;
-        for (i = offset; i < len; i++) {
-            count++;
-            pt = buffer[i];
-            totalX += pt.x;
-            totalY += pt.y;
+    _onMouseMove(e) {
+        console.log("onMouseMove");
+        if (this._path) {
+            this._appendToBuffer(this._getMousePosition(e));
+            this._updateSvgPath();
         }
+    }
+
+    _onMouseUp(e) {
+        console.log("onMouseUp");
+        if (this._path) this._path = null;
+    }
+
+    _getMousePosition(e) {
         return {
-            x: totalX / count,
-            y: totalY / count
+            x: e.pageX - this._rect.left,
+            y: e.pageY - this._rect.top
         };
     }
-    return null;
-};
 
-var updateSvgPath = function () {
-    var pt = getAveragePoint(0);
-
-    if (pt) {
-        // Get the smoothed part of the path that will not change
-        strPath += " L" + pt.x + " " + pt.y;
-
-        // Get the last part of the path (close to the current mouse position)
-        // This part will change if the mouse moves again
-        var tmpPath = "";
-        for (var offset = 2; offset < buffer.length; offset += 2) {
-            pt = getAveragePoint(offset);
-            tmpPath += " L" + pt.x + " " + pt.y;
+    _appendToBuffer(pt) {
+        this._mouseBuffer.push(pt);
+        while (this._mouseBuffer.length > this.mouseBufferSize) {
+            this._mouseBuffer.shift();
         }
-
-        // Set the complete current path coordinates
-        path.setAttribute("d", strPath + tmpPath);
     }
-};
+
+    /** Calculate the average point, starting at offset in the buffer */
+    _getAveragePoint(offset) {
+        var len = this._mouseBuffer.length;
+        if (len % 2 === 1 || len >= this.mouseBufferSize) {
+            var totalX = 0;
+            var totalY = 0;
+            var pt, i;
+            var count = 0;
+            for (i = offset; i < len; i++) {
+                count++;
+                pt = this._mouseBuffer[i];
+                totalX += pt.x;
+                totalY += pt.y;
+            }
+            return {
+                x: totalX / count,
+                y: totalY / count
+            };
+        }
+        return null;
+    }
+
+    _updateSvgPath() {
+        var pt = this._getAveragePoint(0);
+
+        if (pt) {
+            // Get the smoothed part of the path that will not change
+            this._pathStroke += " L" + pt.x + " " + pt.y;
+
+            // Get the last part of the path (close to the current mouse position)
+            // This part will change if the mouse moves again
+            var tmpPath = "";
+            for (var offset = 2; offset < this._mouseBuffer.length; offset += 2) {
+                pt = this._getAveragePoint(offset);
+                tmpPath += " L" + pt.x + " " + pt.y;
+            }
+
+            // Set the complete current path coordinates
+            this._path.setAttribute("d", this._pathStroke + tmpPath);
+        }
+    }
+
+}
+
+class SVGCanvasFactory {
+
+    // static DEFAULT_CANVAS_HEIGHT = "400px";
+    // static DEFAULT_CANVAS_WIDTH = "600px";
+
+    static newDefault() {
+        let canvas = new SVGCanvas(document.getElementById("svgElement"));
+        canvas.mouseBufferSize = document.getElementById("cmbBufferSize").value;
+        console.log(canvas.mouseBufferSize);
+        return canvas;
+    }
+}
+
+window.myCanvas = SVGCanvasFactory.newDefault();
