c09036b4ed7d2e05f9048e4f1fe67b7463a0342d
dominiksta <dominik.stahmer@posteo.de>
2022-01-27 23:26:36 +0100

    fix: no extra logger output for numbers

--- src/util/Logging.ts
+++ src/util/Logging.ts
@@ -23,24 +23,28 @@ class Logger {
 
         let date = (new Date()).toISOString();
 
+        // Dealing with objects in the browser console window is a decent way of
+        // debugging and I don't want to lose that.
+        const objType = (obj: Object): boolean => {
+            return typeof(obj) !== "string" && typeof(obj) !== "number"
+        }
+
         switch(level) {
             case LogLevel.DEBUG:
                 console.debug(`${date} DEBUG: ${obj}`);
-                // Dealing with objects in the browser console window is a
-                // decent way of debugging and I don't want to lose that.
-                if (!(typeof(obj) === "string")) console.debug(obj);
+                if (objType(obj)) console.debug(obj);
                 break;
             case LogLevel.INFO:
                 console.log(`${date} INFO: ${obj}`);
-                if (!(typeof(obj) === "string")) console.log(obj);
+                if (objType(obj)) console.log(obj);
                 break;
             case LogLevel.WARN:
                 console.warn(`${date} WARN: ${obj}`);
-                if (!(typeof(obj) === "string")) console.warn(obj);
+                if (objType(obj)) console.warn(obj);
                 break;
             case LogLevel.ERROR:
                 console.error(`${date} ERROR: ${obj}`);
-                if (!(typeof(obj) === "string")) console.error(obj);
+                if (objType(obj)) console.error(obj);
                 break;
         }
     }
